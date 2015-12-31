2d33b0ecf3851baafd9f4760a32d9278505054de
Dmitry Korolev <dk@mustdigital.ru>
2015-12-31 14:04:36 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@
     'use strict';
 
     var gulp            = require('gulp'),
-        autoprefixer    = require('gulp-autoprefixer'),
+        autoprefixer    = require('autoprefixer');
         less            = require('gulp-less'),
         notify          = require('gulp-notify'),
         postcss         = require('gulp-postcss'),
@@ -27,14 +27,13 @@
             .on('error', notify.onError(function (error) {
                 return 'Failed to Compile LESS: ' + error.message;
             }))
-            // Uncomment this to combine media queries
-            // .pipe(postcss([
-            //     mqpacker
-            // ]))
-            .pipe(autoprefixer({
-                browsers: ['last 3 versions'],
-                cascade: false
-            }))
+            .pipe(postcss([
+                mqpacker,
+                autoprefixer({
+                    browsers: ['last 3 versions'],
+                    cascade: false
+                })
+            ]))
             .pipe(gulp.dest('./tests/'))
             .pipe(notify("LESS Compiled Successfully :)"));
     });
